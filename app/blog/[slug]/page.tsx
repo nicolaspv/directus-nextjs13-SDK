68ff3b2b0d202b7325832520fd8ee8985f2482af
Nicolás Peré Vignau <41745805+nicolaspv@users.noreply.github.com>
2023-08-24 23:05:30 -0300

    Add Next.js 13 SDK example files
    Add Next.js 13 example boilerplate with Javascript SDK files

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import directus from 'lib/directus';
+import { notFound } from 'next/navigation';
+import { readItem } from '@directus/sdk/rest';
+
+async function getPost(slug) {
+  try {
+    const post = await directus.request(
+      readItem('posts', slug, {
+        fields: ['*', { relation: ['*'] }],
+      })
+    );
+    return post;
+  } catch (error) {
+    notFound();
+  }
+}
+
+export default async function DynamicPage({ params }) {
+  const post = await getPost(params.slug);
+  return (
+    <>
+      <img
+        src={`${directus.url}assets/${post.image.filename_disk}?width=600`}
+        alt=''
+      />
+      <h1>{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+    </>
+  );
+}
